1b92062a4871abf8cc4226341817148e4ddc99f3
Ihwan <mynameisihwan@gmail.com>
2020-08-02 00:29:23 +0700

    create new project

---
+++ Learning-CloudKit/Learning-CloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning-CloudKit
+//
+//  Created by Ihwan ID on 02/08/20.
+//  Copyright © 2020 Ihwan ID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
